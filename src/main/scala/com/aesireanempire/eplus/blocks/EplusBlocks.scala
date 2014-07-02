50ed6814a677f8777ed2605d9db8504e59c97651
freyjadono <mike.stengel@gmail.com>
2014-07-02 03:49:29 -0400

    Register TileEntities

--- src/main/scala/com/aesireanempire/eplus/blocks/EplusBlocks.scala
+++ src/main/scala/com/aesireanempire/eplus/blocks/EplusBlocks.scala
@@ -1,21 +1,22 @@
 package com.aesireanempire.eplus.blocks
 
+import com.aesireanempire.eplus.blocks.entities.TileEntityAdvEnchantmentTable
 import cpw.mods.fml.common.registry.GameRegistry
-import net.minecraft.block.Block
 
 
 object EplusBlocks {
 
-  /**
-   * Creates any block objects that need to be initialized.
-   */
-  def preInit() = {
-    GameRegistry.registerBlock(BlockAdvEnchantmentTable, BlockAdvEnchantmentTable.getUnlocalizedName)
-  }
+    /**
+     * Creates any block objects that need to be initialized.
+     */
+    def preInit() = {
+        GameRegistry.registerBlock(BlockAdvEnchantmentTable, BlockAdvEnchantmentTable.getUnlocalizedName)
+        GameRegistry.registerTileEntity(classOf[TileEntityAdvEnchantmentTable], "eplus:advEnchantmentTable")
+    }
 
-  /**
-   * Registers all recipes needed for blocks
-   */
-  def init() = {}
+    /**
+     * Registers all recipes needed for blocks
+     */
+    def init() = {}
 
 }
