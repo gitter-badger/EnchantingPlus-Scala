301b8381455d85c8f0373a6f0b826b2089f19810
freyjadono <mike.stengel@gmail.com>
2014-07-02 08:25:59 -0400

    Tweaked parameters for GUIs

--- src/main/scala/com/aesireanempire/eplus/AdvEnchantmentHelper.scala
+++ src/main/scala/com/aesireanempire/eplus/AdvEnchantmentHelper.scala
@@ -1,5 +1,24 @@
 package com.aesireanempire.eplus
 
+import net.minecraft.enchantment.{Enchantment, EnchantmentHelper}
+import net.minecraft.item.ItemStack
+
+import scala.collection.JavaConversions._
+
 object AdvEnchantmentHelper {
+    def buildEnchantmentList(itemStack: ItemStack): Array[Enchantment] = {
+        val enchantmentsOnItem = EnchantmentHelper.getEnchantments(itemStack).keySet().toList
+
+        val possibleEnchantments: Array[Enchantment] = Enchantment.enchantmentsList.filter(e => e != null && e
+            .canApplyAtEnchantingTable(itemStack))
+
+        for (id <- enchantmentsOnItem) {
+            val enchantment: Enchantment = Enchantment.enchantmentsList(id.asInstanceOf[Int])
+
+            possibleEnchantments.filter(e => e.canApplyTogether(enchantment))
+        }
+
+        possibleEnchantments
+    }
 
 }
--- src/main/scala/com/aesireanempire/eplus/handlers/GUIHandler.scala
+++ src/main/scala/com/aesireanempire/eplus/handlers/GUIHandler.scala
@@ -1,5 +1,6 @@
 package com.aesireanempire.eplus.handlers
 
+import com.aesireanempire.eplus.blocks.entities.TileEntityAdvEnchantmentTable
 import com.aesireanempire.eplus.{ContainerAdvEnchantment, GUIAdvEnchantment}
 import cpw.mods.fml.common.network.IGuiHandler
 import net.minecraft.entity.player.EntityPlayer
@@ -7,14 +8,16 @@ import net.minecraft.world.World
 
 object GUIHandler extends IGuiHandler {
     override def getServerGuiElement(ID: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int): AnyRef = {
+        val te = world.getTileEntity(x, y, z)
         ID match {
-            case 0 => new GUIAdvEnchantment(new ContainerAdvEnchantment())
+            case 0 => new ContainerAdvEnchantment(player, te.asInstanceOf[TileEntityAdvEnchantmentTable])
         }
     }
 
     override def getClientGuiElement(ID: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int): AnyRef = {
+        val te = world.getTileEntity(x, y, z)
         ID match {
-            case 0 => new ContainerAdvEnchantment()
+            case 0 => new GUIAdvEnchantment(player, te.asInstanceOf[TileEntityAdvEnchantmentTable])
         }
     }
 }
