080a8dbcbdc7e7449c808bae9335837c612f6ee9
freyjadono <mike.stengel@gmail.com>
2014-07-02 08:25:29 -0400

    Adjustable sized elements implemented

--- src/main/scala/com/aesireanempire/eplus/GUIAdvEnchantment.scala
+++ src/main/scala/com/aesireanempire/eplus/GUIAdvEnchantment.scala
@@ -1,5 +1,128 @@
 package com.aesireanempire.eplus
 
-class GUIAdvEnchantment {
+import com.aesireanempire.eplus.blocks.entities.TileEntityAdvEnchantmentTable
+import net.minecraft.client.gui.Gui
+import net.minecraft.client.gui.inventory.GuiContainer
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.util.ResourceLocation
+import org.lwjgl.opengl.GL11
+
+
+class GUIAdvEnchantment(player: EntityPlayer, tile: TileEntityAdvEnchantmentTable) extends GuiContainer(new
+        ContainerAdvEnchantment(player, tile)) {
+
+    private final val TEXTURE: ResourceLocation = new ResourceLocation("eplus:textures/gui/enchant_classic.png")
+    private final val ELEMENTS_TEXTURE = new ResourceLocation("eplus:textures/gui/enchant_elements.png")
+
+    private var elements = List.empty[GuiElement]
+
+    private val debug = false
+
+    override def initGui(): Unit = {
+        xSize = 235
+        ySize = 182
+
+        super.initGui()
+
+        val scrollBar = new ScrollBar(guiLeft + 206, guiTop + 16, 12, 72, ELEMENTS_TEXTURE)
+        val listBox = new ListBox(guiLeft + 61, guiTop + 16, 144, 72, ELEMENTS_TEXTURE)
+
+        elements = scrollBar :: listBox :: List.empty
+
+    }
+
+    override def drawGuiContainerBackgroundLayer(f: Float, x: Int, y: Int): Unit = {
+        drawTextureBackground()
+
+        elements.foreach(e => e.draw())
+
+        if (debug) {
+            for (element <- elements) {
+                Gui.drawRect(element.posX, element.posY, element.posX + element.width, element.posY + element.height,
+                    0x8800aa00)
+            }
+        }
+    }
+
+
+    override def mouseMovedOrUp(x: Int, y: Int, event: Int): Unit = {
+        super.mouseMovedOrUp(x, y, event)
+
+        if (debug) {
+            val guiElements: List[GuiElement] = elements.filter(e => e.dragging)
+            if (guiElements.nonEmpty) {
+                val element = guiElements(0)
+
+                element.setDimension(x - element.posX, y - element.posY)
+
+                if (event != -1) {
+                    element.dragging = false
+                }
+            }
+        }
+    }
+
+    override def mouseClicked(x: Int, y: Int, z: Int): Unit = {
+        super.mouseClicked(x, y, z)
+
+        if(debug) {
+            val guiElements: List[GuiElement] = elements.filter(e => e.dragging)
+            if (guiElements.isEmpty) {
+
+                val elementClicked = getElementUnderClick(x, y)
+
+                if (elementClicked != null) {
+                    elementClicked.dragging = true
+                }
+            }
+        }
+    }
+
+    def getContainer: ContainerAdvEnchantment = {
+        inventorySlots.asInstanceOf[ContainerAdvEnchantment]
+    }
+
+    private def getElementUnderClick(x: Int, y: Int): GuiElement = {
+        for (element <- elements) {
+            if (element.posX <= x && x <= element.posX + element.width) {
+                if (element.posY <= y && y <= element.posY + element.height)
+                    return element
+            }
+        }
+        null
+    }
+
+    private def drawTextureBackground() {
+        GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f)
+        mc.renderEngine.bindTexture(TEXTURE)
+        drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize)
+    }
+
+    class ScrollBar(posX: Int, posY: Int, width: Int, height: Int, texture: ResourceLocation) extends GuiElement(posX,
+        posY, width, height, 48, texture) {
+        override def draw() {
+            GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f)
+            mc.renderEngine.bindTexture(texture)
+
+            drawTop()
+            drawMiddle()
+            drawBottom()
+
+            mc.renderEngine.bindTexture(TEXTURE)
+            drawTexturedModalRect(posX, posY, 0, 182, 12, 15)
+        }
+    }
+
+    class ListBox(posX: Int, posY: Int, width: Int, height: Int, texture: ResourceLocation) extends GuiElement(posX,
+        posY, width, height, 0, texture) {
+        override def draw(): Unit = {
+            GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f)
+            mc.renderEngine.bindTexture(texture)
+
+            drawTop()
+            drawMiddle()
+            drawBottom()
+        }
+    }
 
 }
--- src/main/scala/com/aesireanempire/eplus/GuiElement.scala
+++ src/main/scala/com/aesireanempire/eplus/GuiElement.scala
@@ -1,6 +1,61 @@
 package com.aesireanempire.eplus
 
-trait GuiElement {
-    this: com.aesireanempire.eplus.GUIAdvEnchantment.ScrollBar =>
+import net.minecraft.client.gui.Gui
+import net.minecraft.util.ResourceLocation
 
+abstract class GuiElement(val posX: Int, val posY: Int, var width: Int, var height: Int, u: Int,
+                          texture: ResourceLocation) extends Gui {
+    def setDimension(newWidth: Int, newHeight: Int): Unit = {
+        width = if (newWidth >= 30) newWidth else 30
+        height = if (newHeight >= 30) newHeight else 30
+    }
+
+    var dragging: Boolean = false
+
+
+    def draw()
+
+    def drawTop() {
+        drawTexturedModalRect(posX, posY, u, 0, if (width >= 15) 15 else width, 15)
+        if (width > 30) {
+            for (x <- posX + 15 to posX + width - 15 by 15) {
+                drawTexturedModalRect(x, posY, u + 15, 0, 15, 15)
+            }
+        }
+
+        if (width >= 30) {
+            drawTexturedModalRect(posX + width - 15, posY, u + 30, 0, 15, 15)
+        }
+    }
+
+    def drawMiddle() {
+        if (height >= 30) {
+            for (y <- posY + 15 to posY + height - 15 by 15) {
+                drawTexturedModalRect(posX, y, u, 15, if (width >= 15) 15 else width, 15)
+
+                if (width > 30) {
+                    for (x <- posX + 15 to posX + width - 15 by 15) {
+                        drawTexturedModalRect(x, y, u + 15, 15, 15, 15)
+                    }
+                }
+                if (width >= 30) {
+                    drawTexturedModalRect(posX + width - 15, y, u + 30, 15, 15, 15)
+                }
+            }
+        }
+    }
+
+    def drawBottom() {
+        val y: Int = posY + height - 15
+        drawTexturedModalRect(posX, y, u, 30, if (width >= 15) 15 else width, 15)
+        if (width > 30) {
+            for (x <- posX + 15 to posX + width - 15 by 15) {
+                drawTexturedModalRect(x, y, u + 15, 30, 15, 15)
+            }
+        }
+
+        if (width >= 30) {
+            drawTexturedModalRect(posX + width - 15, y, u + 30, 30, 15, 15)
+        }
+    }
 }
