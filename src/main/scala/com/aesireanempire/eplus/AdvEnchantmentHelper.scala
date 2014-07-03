346b9a9748f75c59932231521c40936631811e06
freyjadono <mike.stengel@gmail.com>
2014-07-03 06:58:33 -0400

    Enchantments now show up for books

--- src/main/scala/com/aesireanempire/eplus/AdvEnchantmentHelper.scala
+++ src/main/scala/com/aesireanempire/eplus/AdvEnchantmentHelper.scala
@@ -1,6 +1,7 @@
 package com.aesireanempire.eplus
 
-import net.minecraft.enchantment.{EnchantmentData, Enchantment, EnchantmentHelper}
+import net.minecraft.enchantment.{Enchantment, EnchantmentData, EnchantmentHelper}
+import net.minecraft.init.Items
 import net.minecraft.item.ItemStack
 
 import scala.collection.JavaConversions._
@@ -9,8 +10,9 @@ object AdvEnchantmentHelper {
     def buildEnchantmentList(itemStack: ItemStack): Array[EnchantmentData] = {
         val enchantmentsOnItem = EnchantmentHelper.getEnchantments(itemStack).keySet().toList
 
-        val possibleEnchantments: Array[Enchantment] = Enchantment.enchantmentsList.filter(e => e != null && e
-            .canApplyAtEnchantingTable(itemStack))
+        val possibleEnchantments: Array[Enchantment] = Enchantment.enchantmentsList.filter {
+            e => e != null && (e.canApplyAtEnchantingTable(itemStack) || (isBook(itemStack) && e.isAllowedOnBooks))
+        }
 
         for (id <- enchantmentsOnItem) {
             val enchantment: Enchantment = Enchantment.enchantmentsList(id.asInstanceOf[Int])
@@ -19,5 +21,11 @@ object AdvEnchantmentHelper {
         }
 
         possibleEnchantments.map(e => new EnchantmentData(e, 0))
+    }
+
+    def isBook(itemStack: ItemStack): Boolean = {
+        if (itemStack == null) return false
+        def item = itemStack.getItem
+        item.equals(Items.book) || item.equals(Items.enchanted_book)
     }
 }
