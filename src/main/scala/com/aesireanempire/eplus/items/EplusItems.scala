7af9a517d0e6d538bb00df30ff0b3f32abf19b48
Freyjadono <mike.stengel@gmail.com>
2014-07-30 07:27:20 -0400

    Recipes and working upgrade

--- src/main/scala/com/aesireanempire/eplus/items/EplusItems.scala
+++ src/main/scala/com/aesireanempire/eplus/items/EplusItems.scala
@@ -1,6 +1,8 @@
 package com.aesireanempire.eplus.items
 
 import cpw.mods.fml.common.registry.GameRegistry
+import net.minecraft.init.{Blocks, Items}
+import net.minecraft.item.ItemStack
 
 object EplusItems {
 
@@ -15,6 +17,14 @@ object EplusItems {
     /**
      * Registers all recipes needed for items
      */
-    def init() = {}
+    def init() = {
+        GameRegistry.addShapedRecipe(
+            new ItemStack(ItemAdvTableUpgrade),
+            "gbg", "o o", "geg",
+            'g'.asInstanceOf[Character], Items.gold_ingot, 'b'.asInstanceOf[Character], Items.writable_book,
+            'o'.asInstanceOf[Character], Blocks.obsidian,
+            'e'.asInstanceOf[Character], Items.ender_eye
+        )
+    }
 
 }
