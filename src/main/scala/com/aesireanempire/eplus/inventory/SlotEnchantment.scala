96edd23b0c2fac1e2acc9175a85db066e9fc48f5
Freyjadono <mike.stengel@gmail.com>
2014-07-31 23:17:24 -0400

    Slot will now only accept enchantable items

--- src/main/scala/com/aesireanempire/eplus/inventory/SlotEnchantment.scala
+++ src/main/scala/com/aesireanempire/eplus/inventory/SlotEnchantment.scala
@@ -1,8 +1,13 @@
 package com.aesireanempire.eplus.inventory
 
 import net.minecraft.inventory.{Container, IInventory, Slot}
+import net.minecraft.item.ItemStack
 
 class SlotEnchantment(container: Container, inventory: IInventory, id: Int, x: Int, y: Int) extends Slot(inventory, id,
     x, y) {
     override def getSlotStackLimit: Int = 1
+
+    override def isItemValid(stack : ItemStack): Boolean = {
+        stack.isItemEnchanted || (stack.isItemEnchantable && stack.getItem.getItemEnchantability > 0)
+    }
 }
