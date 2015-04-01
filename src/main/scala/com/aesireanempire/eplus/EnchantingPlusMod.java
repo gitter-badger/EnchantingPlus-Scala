810ad4fab5633bc93996b54c1e36a1aca1e10af4
Darren Boon <darren.boon@rogers.com>
2015-04-01 18:12:42 -0400

    Updating version

--- src/main/scala/com/aesireanempire/eplus/EnchantingPlusMod.java
+++ src/main/scala/com/aesireanempire/eplus/EnchantingPlusMod.java
@@ -10,7 +10,7 @@ public class EnchantingPlusMod {
 
     public static final String MODNAME = "Enchanting Plus";
     public static final String MODID = "eplus";
-    public static final String VERSION = "3.1.0a1";
+    public static final String VERSION = "3.1.0a2";
 
     @SidedProxy(clientSide = "com.aesireanempire.eplus.network.ClientProxy", serverSide = "com.aesireanempire.eplus.network.CommonProxy")
     public static CommonProxy proxy  = null;
